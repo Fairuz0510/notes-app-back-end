3d6b600c7700ae17745e04d3f23da50948418716
fairuz <fairuz.satria@ui.ac.id>
2022-08-13 00:56:00 +0700

    data validation & plugin

--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const nanoid = require('nanoid');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
+  const notesService = new NotesService(nanoid);
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-      cors: true,
+      cors: {
+        origin: ['*'],
+      },
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
